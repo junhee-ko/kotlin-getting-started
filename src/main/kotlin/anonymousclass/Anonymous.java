e81feffe46a680ba7868ded64afe0d9bcb8a1ac7
junhee-ko <junheee.ko@gmail.com>
2021-08-26 00:09:32 +0900

    Add anonymous class

---
+++ src/main/kotlin/anonymousclass/Anonymous.java
@@ -0,0 +1,19 @@
+package anonymousclass;
+
+import java.awt.*;
+
+public class Anonymous {
+
+    public static void main(String[] args) {
+
+//        button.setOnClickListener(new onClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // event when clicked
+//            }
+//        });
+
+//        button.setOnClickListener { /*event when clicked*/ }
+
+    }
+}

f91a838c5d46b06267dfc0a60d247888f5e2b952
junhee-ko <junheee.ko@gmail.com>
2021-08-26 00:11:12 +0900

    Add lamda variable

---
+++ src/main/kotlin/lamdavariable/externalvariable.kt
@@ -0,0 +1,16 @@
+package lamdavariable
+
+fun printProblemCounts(responses: Collection<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+
+    responses.forEach {
+        if (it.startsWith("4")) {
+            clientErrors++
+        } else if (it.startsWith("5")) {
+            serverErrors++
+        }
+    }
+
+    println("client errors : $clientErrors, server erros : $serverErrors")
+}

3de494dcab2e3ac32a5ac94cd554c9bacee2c0e3
junhee-ko <junheee.ko@gmail.com>
2021-08-27 00:47:19 +0900

    Add let

---
+++ src/main/kotlin/nullexample/let.kt
@@ -0,0 +1,13 @@
+package nullexample
+
+fun main() {
+//    val email: String? = ...
+//    if (email != null) {
+//        sendEmailTo(email)
+//    }
+//    email?.let{ sendEmailTo(it)}
+}
+
+fun sendEmailTo(email: String) {
+
+}

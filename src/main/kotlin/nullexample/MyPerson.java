020aff14de4fc7e4bda33dcc12b974a7a668be1e
junhee-ko <junheee.ko@gmail.com>
2021-08-27 00:47:38 +0900

    Add java MyPerson class

---
+++ src/main/kotlin/nullexample/MyPerson.java
@@ -0,0 +1,13 @@
+package nullexample;
+
+public class MyPerson {
+    private final String name;
+
+    public MyPerson(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
